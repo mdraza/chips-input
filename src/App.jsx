781c15a7baacd8b8a482f92f74c31e1ff06366a2
mdraza <raza.uidev@gmail.com>
2025-09-01 16:07:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [chips, setChips] = useState(["coding", "reading"]);
+
+  const handleAddChip = (e) => {
+    if (e.key === "Enter") {
+      setChips((prev) => [...prev, input]);
+      setInput("");
+    }
+  };
+
+  const handleDelete = (index) => {
+    const copyChip = [...chips];
+    copyChip.splice(index, 1);
+    console.log(copyChip);
+    setChips(copyChip);
+  };
+
+  return (
+    <div className="main">
+      <h2>Tags</h2>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => handleAddChip(e)}
+      />
+      <div className="chips">
+        {chips.map((cp, index) => (
+          <div key={index} className="chip">
+            {cp}
+            <button onClick={() => handleDelete(index)}>x</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
